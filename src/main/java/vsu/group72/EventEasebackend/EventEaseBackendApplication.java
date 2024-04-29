8a3f60c0a863059484f4265b7a30ac35c10e2e83
egor_egor <egorgolovin1@mail.ru>
2024-04-29 22:35:00 +0300

    structure created by Spring Initializer

---
+++ src/main/java/vsu/group72/EventEasebackend/EventEaseBackendApplication.java
@@ -0,0 +1,13 @@
+package vsu.group72.EventEasebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventEaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventEaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vsu/group72/EventEasebackend/EventEaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vsu.group72.EventEasebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventEaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
